959f391a534e546dd63b3661faf7b8a9a5db4436
Tilo Mitra <tilomitra@gmail.com>
2012-01-28 21:51:53 -0500

    Initial Commit

---
+++ RestObjC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  RestObjC
+//
+//  Created by Tilo Mitra on 12-01-28.
+//  Copyright (c) 2012 University of Waterloo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main (int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
